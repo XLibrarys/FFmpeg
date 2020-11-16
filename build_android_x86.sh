64c124f617a77c9afcc0350a5f5097e61facbf5e
ChengXiang.Peng <106783860@qq.com>
2020-11-17 02:27:46 +0800

    提交Anroid构建脚本和Configure配置变化

---
+++ build_android_x86.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+NDK=/Users/qitmac0000562/Library/Android/sdk/ndk-bundle
+TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/darwin-x86_64
+API=29
+function build_android
+{
+echo "Compiling FFmpeg for $CPU"
+./configure \
+    --prefix=$PREFIX \
+    --disable-neon \
+    --disable-hwaccels \
+    --disable-gpl \
+    --disable-postproc \
+    --enable-shared \
+    --enable-jni \
+    --disable-mediacodec \
+    --disable-decoder=h264_mediacodec \
+    --disable-static \
+    --disable-doc \
+    --disable-ffmpeg \
+    --disable-ffplay \
+    --disable-ffprobe \
+    --disable-avdevice \
+    --disable-doc \
+    --disable-symver \
+    --cross-prefix=$CROSS_PREFIX \
+    --target-os=android \
+    --arch=$ARCH \
+    --cpu=$CPU \
+    --cc=$CC
+    --cxx=$CXX
+      --enable-cross-compile \
+    --sysroot=$SYSROOT \
+    --extra-cflags="-Os -fpic $OPTIMIZE_CFLAGS" \
+    --extra-ldflags="$ADDI_LDFLAGS" \
+    $ADDITIONAL_CONFIGURE_FLAG
+make clean
+make
+make install
+echo "The Compilation of FFmpeg for $CPU is completed"
+}
+#x86
+ARCH=x86
+CPU=x86
+CC=$TOOLCHAIN/bin/i686-linux-android$API-clang
+CXX=$TOOLCHAIN/bin/i686-linux-android$API-clang++
+SYSROOT=$NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot
+CROSS_PREFIX=$TOOLCHAIN/bin/i686-linux-android-
+PREFIX=$(pwd)/android/$CPU
+OPTIMIZE_CFLAGS="-march=i686 -mtune=intel -mssse3 -
+mfpmath=sse -m32"
+build_android
